6ab1e9db8712688b57c6f27293ebcb1600999ed1
Jared Rand <jjrand96@gmail.com>
2020-11-07 09:33:47 -0500

    Add more data analysis

--- analyse_stock.py
+++ analyse_stock.py
@@ -11,6 +11,10 @@ import matplotlib.pyplot as plt
 
 def main(stock: "The training data file"):
     stock_filename = "./data/daily_" + stock + ".csv"
+    figures_dir = "figures/"
+
+    if not os.path.exists(figures_dir):
+        os.mkdir(figures_dir)
 
     df = pd.read_csv(stock_filename, index_col = 0)
     print(df.info())
@@ -18,7 +22,7 @@ def main(stock: "The training data file"):
     
     print(df.describe())
 
-
+    # Display figures regarding the opening, closing, low, and high per day
     plt.figure(figsize=(15, 5))
     plt.subplot(1,2,1)
     plt.plot(df.open.values, color='red', label='Open')
@@ -29,14 +33,17 @@ def main(stock: "The training data file"):
     plt.xlabel('Time [days]')
     plt.ylabel('Price')
     plt.legend(loc='best')
+    plt.savefig(figures_dir + "/" + stock + "_changes.png")
     plt.show()
 
+    # Display figures regarding the volume
     plt.subplot(1,2,2)
     plt.plot(df.volume.values, color='black', label='Volume')
     plt.title(stock + ' stock volume')
     plt.xlabel('Time [days]')
     plt.ylabel('Volume')
     plt.legend(loc='best')
+    plt.savefig(figures_dir + "/" + stock + "_volume.png")
     plt.show()
 
     # function for min-max normalization of stock
@@ -59,6 +66,7 @@ def main(stock: "The training data file"):
     df_stock_norm = df_stock.copy()
     df_stock_norm = normalize_data(df_stock_norm)
 
+    # Display figures regarding the normalized price and volume
     plt.figure(figsize=(15, 5))
     plt.plot(df_stock_norm.open.values, color='red', label='open')
     plt.plot(df_stock_norm.close.values, color='green', label='low')
@@ -69,8 +77,43 @@ def main(stock: "The training data file"):
     plt.xlabel('Time [days]')
     plt.ylabel('Normalized price/volume')
     plt.legend(loc='best')
+    plt.savefig(figures_dir + "/" + stock + "_normalized.png")
     plt.show()
 
+    # Display histograms of the stock information
+    df.hist(figsize=(12, 12))
+    plt.title(stock + ' histograms')
+    plt.savefig(figures_dir + "/" + stock + "_histograms.png")
+    plt.show()
+
+    # Display figures regarding the moving average
+    ma_day = [10, 20, 50]
+
+    df_stock_ma = df.copy()
+
+    ma_cols = list()
+    ma_cols.append('close')
+
+    for ma in ma_day:
+        column_name = f"MA for {ma} days"
+        ma_cols.append(column_name)
+        df_stock_ma[column_name] = df_stock_ma['close'].rolling(ma).mean()
+
+    df_stock_ma[ma_cols].plot()
+    plt.title(stock + ' moving averages')
+    plt.savefig(figures_dir + "/" + stock + "_moving_average.png")
+    plt.show()
+
+    df_stock_ma['Daily Return'] = df_stock_ma['close'].pct_change()
+    df_stock_ma['Daily Return'].plot(legend=True, linestyle='--', marker='o')
+    plt.title(stock + ' daily return')
+    plt.savefig(figures_dir + "/" + stock + "_daily_return.png")
+    plt.show()
+
+    df_stock_ma['Daily Return'].hist()
+    plt.title(stock + ' daily return histogram')
+    plt.savefig(figures_dir + "/" + stock + "_daily_return_histogram.png")
+    plt.show()
 
 if __name__ == "__main__":
     import plac
